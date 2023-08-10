9abf0892880854f5efacd147a839d97578841d98
Brad Allgood <brad.allgood.personal@gmail.com>
2023-08-10 13:34:54 -0500

    update for multiple years and weeks

--- sql_app/main.py
+++ sql_app/main.py
@@ -40,7 +40,6 @@ async def root():
 
 @app.get("/passing/", response_model=list[schemas.Passing])
 def read_passing(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    print('Hello World')
     passing = crud.get_passing(db, skip=skip, limit=limit)
     return passing
 
@@ -48,32 +47,3 @@ def read_passing(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 def read_passing_yds(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     passing = crud.get_passing_yds(db, skip=skip, limit=limit)
     return passing
-
-#@app.get("/passing_yds/", response_model=list[schemas.Passing])
-#def read_passing_yds(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#    passing_yds = crud.get_passing_yds(db, skip=skip, limit=limit)
-#    return passing_yds
-
-@app.get("/test/")
-async def test():
-    return {"message": "This is test"}
-
-#@app.get("/users/{user_id}", response_model=schemas.User)
-#def read_user(user_id: int, db: Session = Depends(get_db)):
-#    db_user = crud.get_user(db, user_id=user_id)
-#    if db_user is None:
-#        raise HTTPException(status_code=404, detail="User not found")
-#    return db_user
-
-
-#@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-#def create_item_for_user(
-#    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-#):
-#    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-#@app.get("/items/", response_model=list[schemas.Item])
-#def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#    items = crud.get_items(db, skip=skip, limit=limit)
-#    return items
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 class PassingBase(BaseModel):
     pass
 
-
 class Passing(PassingBase):
     RK: int
     TEAM: str
@@ -26,8 +25,9 @@ class Passing(PassingBase):
     WINNER_SCORE: int 
     LOSER_SCORE: int  
     SPREAD: int       
-    YEAR: str
-    WEEK: str 
+    YEAR: int
+    WEEK: int
+    PKCOL: str 
 
     class Config:
         orm_mode = True
